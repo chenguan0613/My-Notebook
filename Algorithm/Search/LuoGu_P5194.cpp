613902a20fb2d7edf51b96ab885b95462ca9aef8
ChenGuan <3168966792@qq.com>
2026-04-23 23:54:35 +0800

    searching

--- Algorithm/Search/LuoGu_P5194.cpp
+++ Algorithm/Search/LuoGu_P5194.cpp
@@ -3,6 +3,7 @@ using namespace std;
 typedef long long ll;
 ll n,c;
 vector<ll> w;
+vector<ll> post_sum;
 ll ans=0;
 void dfs(int u,ll current_w){
     if(current_w>c) return;
@@ -10,13 +11,17 @@ void dfs(int u,ll current_w){
         ans=max(ans,current_w);
         return;
     }
+    if(current_w+post_sum[u]<=ans) return;
     dfs(u+1,current_w);
     if(current_w+w[u]<=c) dfs(u+1,current_w+w[u]);
 }
 int main(){
     cin>>n>>c;
     w.resize(n);
+    post_sum.resize(n+1);
+    post_sum[n]=0;
     for(int i=0;i<n;i++) cin>>w[i];
+    for(int i=n-1;i>=0;i--) post_sum[i]=post_sum[i+1]+w[i];
     dfs(0,0);
     cout<<ans<<endl;
     return 0;
